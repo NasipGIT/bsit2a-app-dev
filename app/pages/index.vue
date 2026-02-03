b404249803c1c47a62500c7f58e0f919a185882d
nasip <nasip.j.solaiman@gmail.com>
2026-02-03 10:25:36 +0800

    added layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
-    <v-btn>SUBMIT</v-btn>
-  </div>
+   <v-row>
+
+      <v-col cols="12" md="3">
+    <v-card height="60" color="primary">
+      <v-text></v-text> </v-card>
+  </v-col>
+
+    <v-col cols="12" md="3">
+    <v-card height="60" color="red"> </v-card>
+  </v-col>
+  
+    <v-col cols="12" md="3">
+    <v-card height="60" color="pink"> </v-card>
+  </v-col>
+
+    <v-col cols="12" md="3">
+    <v-card height="60" color="orange"> </v-card>
+  </v-col>
+
+  </v-row>
+
+  
+</div>
 </template>
 
 <script lang="ts" setup>
